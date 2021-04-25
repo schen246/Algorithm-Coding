344b9bb5242c9dd8baef3562eb7eff8a598d172b
Sai Chen <saichenlaioffer@gmail.com>
2021-04-24 22:27:41 -0400

    add BinaryTreePreorderTraversal.java

---
+++ BinaryTreePreorderTraversal.java
@@ -0,0 +1,20 @@
+public class BinaryTreePreorderTraversal {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        pushLeft(root, stack, res);
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            pushLeft(cur.right, stack, res);
+        }
+        return res;
+    }
+    
+    private void pushLeft(TreeNode root, Deque<TreeNode> stack, List<Integer> res) {
+        while (root != null) {
+            res.add(root.val);
+            stack.push(root);
+            root = root.left;
+        }
+    }
+}

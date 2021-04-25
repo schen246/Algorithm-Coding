ec2fc71db4d827b01f26da5e822a3155e55968ae
Sai Chen <saichenlaioffer@gmail.com>
2021-04-24 22:10:46 -0400

    add BinaryTreeInorderTraversal.java

---
+++ BinaryTreeInorderTraversal.java
@@ -0,0 +1,32 @@
+class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class BinaryTreeInorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        // go through left
+        pushLeft(root, stack);
+        // traverse
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            res.add(cur.val);
+            pushLeft(cur.right);
+        }
+        return res;
+    }
+
+    private void pushLeft(TreeNode root, Deque<TreeNode> stack) {
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+    }
+}
+

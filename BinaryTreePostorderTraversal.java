63c24acc42b6131b33a57e60de795ef2b990685d
Sai Chen <saichenlaioffer@gmail.com>
2021-04-24 22:55:30 -0400

    add BinaryTreePostorderTraversal.java

---
+++ BinaryTreePostorderTraversal.java
@@ -0,0 +1,25 @@
+public class BinaryTreePostorderTraversal {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        pushLeft(root, stack);
+        TreeNode prev = null;
+        while (!stack.isEmpty()) {
+            if (stack.peek().right != null && stack.peek().right != prev) {
+                pushLeft(stack.peek().right, stack);
+            } else {
+                TreeNode cur = stack.pop();
+                res.add(cur.val);
+                prev = cur;
+            }
+        }
+        return res;
+    }
+
+    private void pushLeft(TreeNode root, Deque<TreeNode> stack) {
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+    }
+}

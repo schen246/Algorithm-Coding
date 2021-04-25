6e668211a8ab17fb58ac303afd299918b76d5b63
Sai Chen <saichenlaioffer@gmail.com>
2021-04-25 10:40:45 -0400

    add searchMatrix.java

---
+++ SearchInA2DMatrix.java
@@ -0,0 +1,22 @@
+public class SearchInA2DMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.legnth == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int m = matrix.length;
+        int n = matrix.length;
+        int left = 0, right = m * n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (matrix[mid / n][mid % n] == target) {
+                return true;
+            }
+            if (matrix[mid / n][mid % n] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
+}

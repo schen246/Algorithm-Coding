ee81182bd3e94bda82811c11709187e645bb1f8a
Sai Chen <saichenlaioffer@gmail.com>
2021-04-25 21:46:33 -0400

    add SearchInsertPosition.java

---
+++ SearchInsertPosition.java
@@ -0,0 +1,19 @@
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int left = 0, right = nums.length - 1;
+        int res = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target) {
+                res = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return res == -1 ? nums.length : res;
+    }
+}

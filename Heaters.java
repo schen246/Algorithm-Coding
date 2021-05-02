f1309029bb7befdb3ea44cc4817f32d55da49618
Sai Chen <saichenlaioffer@gmail.com>
2021-05-02 01:18:30 -0400

    add Heaters.java

---
+++ Heaters.java
@@ -0,0 +1,28 @@
+public class Heaters {
+    // time: O(heaters.length * log(heaters.length) + houses.length * log(heaters.length))
+    public int findRadius(int[] houses, int[] heaters) {
+        Arrays.sort(heaters);
+        int res = 0;
+        for (int house : houses) {
+            int index = smallestLargerOrEqualThanTarget(heaters, house);
+            int dist1 = index - 1 >= 0 ? house - heaters[index - 1] : Integer.MAX_VALUE;
+            int dist2 = index < heaters.length ? heaters[index] - house : Integer.MAX_VALUE;
+            res = Math.max(res, Math.min(dist1, dist2));
+        }
+        return res;
+    }
+
+    private int smallestLargerOrEqualThanTarget(int[] arr, int target) {
+        int left = 0, right = arr.length - 1, res = arr.length;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] >= target) {
+                res = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return res;
+    }
+}

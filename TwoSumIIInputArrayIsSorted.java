01ca32d0e769bd08f5b2c09594c1d52a4827db24
Sai Chen <saichenlaioffer@gmail.com>
2021-05-02 18:32:46 -0400

    add TwoSumIIInputArrayIsSorted.java

---
+++ TwoSumIIInputArrayIsSorted.java
@@ -0,0 +1,19 @@
+public class TwoSumIIInputArrayIsSorted {
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length < 2) {
+            return new int[]{-1, -1};
+        }
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] + nums[right] == target) {
+                return new int[]{left + 1, right + 1};
+            }
+            if (nums[left] + nums[right] < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}

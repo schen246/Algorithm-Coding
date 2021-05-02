d93ec8c01ffd4aa1750b91efa9ecde1661423056
Sai Chen <saichenlaioffer@gmail.com>
2021-05-01 23:53:33 -0400

    add MissingElementInSortedArray.java

---
+++ MissingElementInSortedArray.java
@@ -0,0 +1,18 @@
+public class MissingElementInSortedArray {
+    public int missingElement(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int left = 0, right = nums.length - 1, res = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] - nums[0] - mid < k) {
+                res = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return nums[res] + (k - (nums[res] - nums[0] - res));
+    }
+}

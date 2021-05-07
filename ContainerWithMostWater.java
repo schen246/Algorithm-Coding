2cbb0c59da915976a3fa585b51444a31ac994f69
Sai Chen <saichenlaioffer@gmail.com>
2021-05-06 23:33:20 -0400

    add ContainerWithMostWater.java

---
+++ ContainerWithMostWater.java
@@ -0,0 +1,17 @@
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        int i = 0, j = height.length - 1, res = 0;
+        while (i < j) {
+            res = Math.max(res, (j - i) * Math.min(height[i], height[j]));
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return res;
+    }
+}

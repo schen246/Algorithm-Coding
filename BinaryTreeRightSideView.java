4a85973c65b60b06cf6fec894b19cd4f574066a0
Sai Chen <saichenlaioffer@gmail.com>
2021-05-08 20:37:44 -0400

    add BinaryTreeRightSideView.java

---
+++ BinaryTreeRightSideView.java
@@ -0,0 +1,21 @@
+public class BinaryTreeRightSideView {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.offer(root);
+        while (q.size() > 0) {
+            int n = q.size();
+            res.add(q.peek().val);
+            for (int i = 0; i < n; i++) {
+                TreeNode node = q.poll();
+                if (node.right != null) q.offer(node.right);
+                if (node.left != null) q.offer(node.left);
+            }
+        }
+        return res;
+    }
+    // time: O(n) space: O(n)
+}

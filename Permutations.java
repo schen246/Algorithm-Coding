bafad3844e55950182adecc08489dad1db252d03
Sai Chen <saichenlaioffer@gmail.com>
2021-05-12 01:36:06 -0400

    add Permutations.java

---
+++ Permutations.java
@@ -0,0 +1,30 @@
+public class Permutations {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(nums, 0, res);
+        return res;
+    }
+
+    private void dfs(int[] nums, int index, List<List<Integer>> res) {
+        if (index == nums.length) {
+            List<Integer> cur = new ArrayList<>();
+            for (int num : nums) {
+                cur.add(num);
+            }
+            res.add(cur);
+            return;
+        }
+        for (int i = index; i < nums.length; i++) {
+            swap(nums, index, i);
+            dfs(nums, index + 1, res);
+            swap(nums, index, i);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+    // time: O(n!) space: O(n)
+}

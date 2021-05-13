17e3dc7c3571d404cb69fa7186362d1912aa4b61
Sai Chen <saichenlaioffer@gmail.com>
2021-05-12 23:49:50 -0400

    add LetterCombinationsOfAPhoneNumber.java

---
+++ LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,26 @@
+public class LetterCombinationsOfAPhoneNumber {
+    private static final String[] PAD = new String[]{"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    
+    public List<String> letterCombinations(String digits) {
+        List<String> res = new ArrayList<>();
+        if (digits == null || digits.length() == 0) {
+            return res;
+        }
+        StringBuilder sb = new StringBuilder();
+        dfs(digits, 0, sb, res);
+        return res;
+    }
+    
+    private void dfs(String digits, int index, StringBuilder sb, List<String> res) {
+        if (index == digits.length()) {
+            res.add(sb.toString());
+            return;
+        }
+        for (char c : PAD[digits.charAt(index) - '0' - 2].toCharArray()) {
+            sb.append(c);
+            dfs(digits, index + 1, sb, res);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+    // time: O(max(digit.length)^digits.length() * digits.length()) space: O(digits.length())
+}

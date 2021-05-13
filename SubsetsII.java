3e44084d61ed73878faf44a483ee8819fdad513d
Sai Chen <saichenlaioffer@gmail.com>
2021-05-12 23:37:41 -0400

    add SubsetsII.java

---
+++ SubsetsII.java
@@ -0,0 +1,27 @@
+public class SubsetsII {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> cur = new ArrayList<>();
+        Arrays.sort(nums);
+        dfs(nums, 0, cur, res);
+        return res;
+    }
+    
+    private void dfs(int[] nums, int index, List<Integer> cur, List<List<Integer>> res) {
+        if (index == nums.length) {
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+        // add
+        cur.add(nums[index]);
+        dfs(nums, index + 1, cur, res);
+        cur.remove(cur.size() - 1);
+        
+        while (index + 1 < nums.length && nums[index + 1] == nums[index]) {
+            index++;
+        }
+        // not add
+        dfs(nums, index + 1, cur, res);
+    }
+    // time: O(2^n) space: O(n)
+}

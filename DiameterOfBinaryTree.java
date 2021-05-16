ddc5358f3f9c58d7d70c7c1ad0580247a2435990
Sai Chen <saichenlaioffer@gmail.com>
2021-05-16 02:31:49 -0400

    add DiameterOfBinaryTree.java

---
+++ DiameterOfBinaryTree.java
@@ -0,0 +1,18 @@
+public class DiameterOfBinaryTree {
+    public int diameterOfBinaryTree(TreeNode root) {
+        int[] res = new int[1];
+        helper(root, res);
+        return res[0];
+    }
+
+    private int helper(TreeNode root, int[] res) {
+        if (root == null) {
+            return 0;
+        }
+        int left = helper(root.left, res);
+        int right = helper(root.right, res);
+        res[0] = Math.max(res[0], left + right);
+        return Math.max(left, right) + 1;
+    }
+    // time: O(n) space: O(height)
+}

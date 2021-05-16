d2d4e0490cb7d05d4b3e72a429a3f6eedca30923
Sai Chen <saichenlaioffer@gmail.com>
2021-05-16 16:50:56 -0400

    add LowestCommonAncestorOfABinaryTree.java

---
+++ LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,14 @@
+public class LowestCommonAncestorOfABinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, int p, int q) {
+        if (root == null || root.val == p || root.val == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        return left == null ? right : left;
+    }
+    // time: O(n) space: O(height)
+}

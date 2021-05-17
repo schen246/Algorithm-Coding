54d110976c06a95224f1d0bcff0b36206e05b775
Sai Chen <saichenlaioffer@gmail.com>
2021-05-16 23:58:50 -0400

    add ClosestBinarySearchTreeValue.java

---
+++ ClosestBinarySearchTreeValue.java
@@ -0,0 +1,20 @@
+public class ClosestBinarySearchTreeValue {
+    public int closestValue(TreeNode root, double target) {
+        if (root == null) {
+            return -1;
+        }
+        int res = root.val;
+        while (root != null) {
+            if (Math.abs(root.val - target) < Math.abs(res - target)) {
+                res = root.val;
+            }
+            if (root.val < target) {
+                root = root.right;
+            } else {
+                root = root.left;
+            }
+        }
+        return res;
+    }
+    // time: O(n) space: O(1)
+}

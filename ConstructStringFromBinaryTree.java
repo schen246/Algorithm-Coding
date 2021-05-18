6c37bcdeca4666dca908b4b86d2f94da3ac72a3d
Sai Chen <saichenlaioffer@gmail.com>
2021-05-18 00:29:33 -0400

    add ConstructStringFromBinaryTree.java

---
+++ ConstructStringFromBinaryTree.java
@@ -0,0 +1,15 @@
+public class ConstructStringFromBinaryTree {
+    public String tree2str(TreeNode t) {
+        if (t == null) {
+            return "";
+        }
+        if (t.left == null && t.right == null) {
+            return t.val + "";
+        }
+        if (t.right == null) {
+            return t.val + "(" + tree2str(t.left) + ")";
+        }
+        return t.val + "(" + tree2str(t.left) + ")(" + tree2str(t.right) + ")";
+    }
+    // time: O(n) space: O(n) worst case
+}

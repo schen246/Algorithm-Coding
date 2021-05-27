a2c69ab62825b771b7c4cc51dca75ae3a617f534
Sai Chen <saichenlaioffer@gmail.com>
2021-05-27 00:28:46 -0400

    add TopKFrequentElements.java

---
+++ TopKFrequentElements.java
@@ -0,0 +1,24 @@
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class TopKFrequentElements {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[1] - b[1]));
+        for (int i : map.keySet()) {
+            pq.offer(new int[]{i, map.get(i)});
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        List<Integer> res = new ArrayList<>();
+        while (pq.size() > 0) {
+            res.add(pq.poll()[0]);
+        }
+        return res;
+    }
+    // time: O(|keys| * logk) space: O(k)
+}

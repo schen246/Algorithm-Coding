c908c01fe3840313645e69f9b68e5d2e1b49a4c8
Sai Chen <saichenlaioffer@gmail.com>
2021-05-27 19:33:51 -0400

    add SortCharactersByFrequency.java

---
+++ SortCharactersByFrequency.java
@@ -0,0 +1,30 @@
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class SortCharactersByFrequency {
+    public String frequencySort(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        PriorityQueue<Character> pq = new PriorityQueue<>((a, b) -> {
+            if (map.get(a).equals(map.get(b))) {
+                return a - b;// lower alphabetical
+            }
+            return map.get(b) - max.get(a);// max heap
+        });
+        for (char c : map.keySet()) {
+            pq.offer(c);
+        }
+        StringBuilder sb = new StringBuilder();
+        while (pq.size() > 0) {
+            char c = pq.poll();
+            int freq = map.get(c);
+            for (int i = 0; i < freq; i++) {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+    // time: O(MlogM + n) space: O(M) not consider StringBuilder
+}

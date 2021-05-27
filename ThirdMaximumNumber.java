fb9cd3966c0cadb351983c23a2bc5146375a6b5e
Sai Chen <saichenlaioffer@gmail.com>
2021-05-27 01:56:24 -0400

    add ThirdMaximumNumber.java

---
+++ ThirdMaximumNumber.java
@@ -0,0 +1,21 @@
+public class ThirdMaximumNumber {
+    public int thirdMax(int[] nums) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) continue;
+            pq.offer(num);
+            if (pq.size() > 3) {
+                pq.poll();
+            }
+        }
+        if (pq.size() == 3) {
+            return pq.peek();
+        }
+        while (pq.size() > 1) {
+            pq.poll();
+        }
+        return pq.peek();
+    }
+    // time: O(nlog3) space: O(3)
+}

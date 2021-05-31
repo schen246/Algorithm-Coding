11a623e660895d12ba9c532acbac0bfa3e8d6d02
Sai Chen <saichenlaioffer@gmail.com>
2021-05-31 17:02:16 -0400

    add ContiguousArray.java

---
+++ ContiguousArray.java
@@ -0,0 +1,24 @@
+import java.util.Map;
+
+public class ContiguousArray {
+    // subarray with equal number of 0 and 1
+    // time: O(n) space: O(n)
+    public int findMaxLength(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int sum = 0, res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                sum -= 1;
+            } else {
+                sum += 1;
+            }
+            if (map.containsKey(sum)) {
+                res = Math.max(res, i - map.get(sum));
+            } else {
+                map.put(sum, i);
+            }
+        }
+        return res;
+    }
+}

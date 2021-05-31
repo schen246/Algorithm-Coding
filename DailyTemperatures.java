d68cc77378164eb8d65d2627d16320c8e5c10a2e
Sai Chen <saichenlaioffer@gmail.com>
2021-05-31 18:03:42 -0400

    add DailyTemperatures.java

---
+++ DailyTemperatures.java
@@ -0,0 +1,17 @@
+import java.util.Deque;
+
+public class DailyTemperatures {
+    // monoStack - time: O(n) space: O(n)
+    public int[] dailyTemperatures(int[] T) {
+        int[] res = new int[T.length];
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < T.length; i++) {
+            while (!stack.isEmpty() && T[stack.peek()] < T[i]) {
+                int index = stack.pop();
+                res[index] = i - index;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}

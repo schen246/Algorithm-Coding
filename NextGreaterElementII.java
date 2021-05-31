743cf598b6535dc7570e6ce3913e5c256f5329a6
Sai Chen <saichenlaioffer@gmail.com>
2021-05-31 17:54:09 -0400

    add NextGreaterElementII.java

---
+++ NextGreaterElementII.java
@@ -0,0 +1,25 @@
+import java.util.Deque;
+
+public class NextGreaterElementII {
+    // monoStack - time: O(n) space: O(n)
+    public int[] nextGreaterElements(int[] nums) {
+        int[] res = new int[nums.length];
+        Arrays.fill(nums, -1);
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < nums.length; i++) {
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[i]) {
+                res[stack.pop()] = nums[i];
+            }
+            stack.push(i);
+        }
+        int i = 0;
+        while (!stack.isEmpty() && i < nums.length) {
+            if (nums[i] > nums[stack.peek()]) {
+                res[stack.pop()] = nums[i];
+            } else {
+                i++;
+            }
+        }
+        return res;
+    }
+}

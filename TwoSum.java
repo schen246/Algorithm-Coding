ead36a175dda279e7a6fceae6294152b558ba1ad
Sai Chen <saichenlaioffer@gmail.com>
2021-06-02 18:02:45 -0400

    add TwoSum.java

---
+++ TwoSum.java
@@ -0,0 +1,15 @@
+import java.util.Map;
+
+public class TwoSum {
+    // M1: map - time: O(n) space: O(n)
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+}

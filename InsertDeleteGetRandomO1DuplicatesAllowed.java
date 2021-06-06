938f6c0e63eb1f819e062fea3894f0d184d13db1
Sai Chen <saichenlaioffer@gmail.com>
2021-06-06 19:58:16 -0400

    add InsertDeleteGetRandomO1DuplicatesAllowed.java

---
+++ InsertDeleteGetRandomO1DuplicatesAllowed.java
@@ -0,0 +1,42 @@
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+public class InsertDeleteGetRandomO1DuplicatesAllowed {
+    Map<Integer, Set<Integer>> map;// val, set of index of val in list
+    List<Integer> list;// val
+    Random random;
+    public InsertDeleteGetRandomO1DuplicatesAllowed() {
+        map = new HashMap<>();
+        list = new ArrayList<>();
+        random = new Random();
+    }
+
+    public boolean insert(int val) {
+        Set<Integer> cur = map.getOrDefault(val, new HashSet<>());
+        cur.add(list.size());
+        map.put(val, cur);
+        list.add(val);
+        return map.get(val).size() == 1;
+    }
+
+    public boolean remove(int val) {
+        if (!map.containsKey(val) || map.get(val).size() == 0) {
+            return false;
+        }
+        int index = map.get(val).iterator().next();
+        map.get(val).remove(index);
+        int lastValue = list.get(list.size() - 1);
+        list.set(index, lastValue);
+        map.get(lastValue).add(index);
+        map.get(lastValue).remove(list.size() - 1);
+        list.remove(list.size() - 1);
+        return true;
+    }
+
+    public int getRandom() {
+        int index = random.nextInt(list.size());
+        return list.get(index);
+    }
+}

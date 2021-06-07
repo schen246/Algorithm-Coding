f71cd5ad31712fd07e7ae33ddc77dcf68578bf0b
Sai Chen <saichenlaioffer@gmail.com>
2021-06-07 00:26:37 -0400

    add MinimumDepthOfBinaryTree.java

---
+++ MinimumDepthOfBinaryTree.java
@@ -0,0 +1,47 @@
+public class MinimumDepthOfBinaryTree {
+    // M1: recursion (divide and conquer) - time: O(n) space: O(height)
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        if (left == 0 && right == 0) {
+            return 1;
+        }
+        if (left == 0) {
+            return right + 1;
+        }
+        if (right == 0) {
+            return left + 1;
+        }
+        return Math.min(left, right) + 1;
+    }
+
+    // M2: BFS - time: O(n) worst case, space: O(height) with shortest path
+    public int minDepth2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.offer(root);
+        int depth = 0;
+        while (q.size() > 0) {
+            int size = q.size();
+            depth++;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                if (node.left == null && node.right == null) {
+                    return depth;
+                }
+                if (node.left != null) {
+                    q.offer(node.left);
+                }
+                if (node.right != null) {
+                    q.offer(node.right);
+                }
+            }
+        }
+        return depth;
+    }
+}

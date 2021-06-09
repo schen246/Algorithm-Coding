32547bf245c06ed71595269cc595fe49e153aacf
Sai Chen <saichenlaioffer@gmail.com>
2021-06-08 23:04:05 -0400

    add NextGreaterNodeInLinkedList.java

---
+++ NextGreaterNodeInLinkedList.java
@@ -0,0 +1,21 @@
+public class NextGreaterNodeInLinkedList {
+    // monoStack - time: O(n) space: O(n)
+    public int[] nextLargerNodes(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        Deque<Integer> stack = new ArrayDeque<>();
+        int[] res = new int[list.size()];
+        int i = 0;
+        while (i < list.size()) {
+            while (!stack.isEmpty() && list.get(stack.peek()) < list.get(i)) {
+                res[stack.pop()] = list.get(i);
+            }
+            stack.push(i);
+            i++;
+        }
+        return res;
+    }
+}

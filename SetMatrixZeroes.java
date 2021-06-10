689ffb30dbb7bc1da3a2aa6192887b388a8dd56a
Sai Chen <saichenlaioffer@gmail.com>
2021-06-10 00:36:31 -0400

    add SetMatrixZeroes.java

---
+++ SetMatrixZeroes.java
@@ -0,0 +1,26 @@
+public class SetMatrixZeroes {
+    // M1: new matrix - O(m * n) space
+    // M2: two sets - O(k) space, k is the number of zeroes
+    public void setZeros(int[][] matrix) {
+        Set<Integer> rows = new HashSet<>();
+        Set<Integer> cols = new HashSet<>();
+        int m = matrix.length, n = matrix[0].length;
+        // find zeros
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    rows.add(i);
+                    cols.add(j);
+                }
+            }
+        }
+        // set zeros
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j <n; j++) {
+                if (rows.contains(i) || cols.contains(j)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}

472b5bca2a68e62eb06820bcc68d3f6df4fbb379
Sai Chen <saichenlaioffer@gmail.com>
2021-06-12 19:34:48 -0400

    add HappyNumber.java

--- GameOfLife.java
+++ GameOfLife.java
@@ -1,10 +1,8 @@
 public class GameOfLife {
     // 2d in place - time: O(m * n) space: O(1)
     public void gameOfLife(int[][] board) {
-        // 0 cur dead future dead
-        // 1 cur live future dead
-        // 2 cur dead future live
-        // 3 cur live future live
+        // -1: 0 -> 1
+        // -2: 1 -> 0
         int m = board.length, n = board[0].length;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
@@ -13,37 +11,35 @@ public class GameOfLife {
         }
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                board[i][j] >>= 1;
+                if (board[i][j] == -1) {
+                    board[i][j] = 1;
+                } else if (board[i][j] == -2) {
+                    board[i][j] = 0;
+                }
             }
         }
     }
-    
-    private static final int[][] DIRS = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+    private static final int[][] DIRS = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
     
     private void nextState(int[][] board, int i, int j) {
         int cnt = 0;
-        if (board[i][j] == 0) {
-            for (int[] dir : DIRS) {
-                int x = i + dir[0], y = j + dir[1];
-                if (x >= 0 && x < board.length && y >= 0 && y < board[0].length) {
-                    if (board[x][y] == 1 || board[x][y] == 3) {
-                        cnt++;
-                    }
+        for (int[] dir : DIRS) {
+            int x = i + dir[0], y = j + dir[1];
+            if (x >= 0 && x < board.length && y >= 0 && y < board[0].length) {
+                if (board[x][y] == 1 || board[x][y] == -2) {
+                    cnt++;
                 }
-            }
-            board[i][j] = cnt == 3 ? 2 : 0;
-        } else {
-            for (int[] dir : DIRS) {
-                int x = i + dir[0], y = j + dir[1];
-                if (x >= 0 && x < board.length && y >= 0 && y < board[0].length) {
-                    if (board[x][y] == 1 || board[x][y] == 3) {
-                        cnt++;
-                    }
-                }
-            }
-            if (cnt == 2 || cnt == 3) {
-                board[i][j] = 3;
             }
         }
+        if (board[i][j] == 0 && cnt == 3) {
+                board[i][j] = -1;   
+        } else if (board[i][j] == 1 && (cnt < 2 || cnt > 3)) {
+                board[i][j] = -2;
+        }
     }
+
+    // Follow Up: if board going to be infinite
+    // 1. computationally impossible to iterate a matrix that large -> map reduce
+    // 2. impossible to store big matrix in memory -> distributed server or three rows each time
+    // 3. waste space if only few live cells within extremely sparse matrix -> save location of live cells and apply 4 rules
 }
---
+++ HappyNumber.java
@@ -0,0 +1,37 @@
+public class HappyNumber {
+    // find circle in a series of number
+    // 2 -> 4 -> 16 -> 37 -> 58 -> 89 -> 145 -> 42 -> 20 -> 4
+    // M1: set - time: O(L) space: O(L)
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        while (n != 1) {
+            int cur = calculate(n);
+            if (!set.add(cur)) {
+                return false;
+            }
+            n = cur;
+        }
+        return true;
+    }
+    
+    private int calculate(int n) {
+        int res = 0;
+        while (n != 0) {
+            int rem = n % 10;
+            n /= 10;
+            res += rem * rem;
+        }
+        return res;
+    }
+
+    // M2: two pointers - time: O(L) space: O(1)
+    public boolean isHappy2(int n) {
+        int s = n;
+        int f = calculate(n);
+        while (f != 1 && f != s) {
+            s = calculate(s);
+            f = calculate(calculate(f));
+        }
+        return f == 1;
+    }
+}

625bbb70135bb4f74eb6c0d4eafec88bee91de88
Sai Chen <saichenlaioffer@gmail.com>
2021-06-13 01:46:08 -0400

    add MinimizeMaxDistanceToGasStation.java

--- MinimizeMaxDistanceToGasStation.java
+++ MinimizeMaxDistanceToGasStation.java
@@ -16,4 +16,31 @@ public class MinimizeMaxDistanceToGasStation {
         }
         return (double)pq.peek()[0] / pq.peek()[1];
     }
+
+    // M2: binary search - time: O(nlogD) space: O(1)
+    public double minmaxGasDist2(int[] stations, int k) {
+        double left = 0.0, right = Double.MIN_VALUE;
+        for (int i = 1; i < stations.length; i++) {
+            right = Math.max(right, (double)stations[i] - stations[i - 1]);
+        }
+        double res = 0.0;
+        while (left + 1e-6 <= right) {// O(log(max interval))
+            double mid = left + (right - left) / 2;
+            if (canAdd(stations, k, mid)) {
+                res = mid;
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+        return res;
+    }
+
+    private boolean canAdd(int[] stations, int k, double penalty) {// O(n)
+        int cnt = 0;
+        for (int i = 1; i < stations.length; i++) {
+            cnt += Math.ceil((stations[i] - stations[i - 1]) / penalty) - 1;
+        }
+        return cnt <= k;
+    }
 }

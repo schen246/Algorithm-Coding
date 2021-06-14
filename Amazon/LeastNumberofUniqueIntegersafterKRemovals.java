8c874124e07f0b7402b09b1a2a2a8e9c81c1aef2
Sai Chen <saichenlaioffer@gmail.com>
2021-06-14 18:43:28 -0400

    add LeastNumberofUniqueIntegersafterKRemovals.java

---
+++ Amazon/LeastNumberofUniqueIntegersafterKRemovals.java
@@ -0,0 +1,20 @@
+public class LeastNumberofUniqueIntegersafterKRemovals {
+    // linked list hashmap - time: O(nlogn) space: O(n) depends on sort
+    public int findLeastNumOfUniqueInts(int[] arr, int k) {
+        Map<Integer, Integer> map = new HashMap<>();// val, freq
+        for (int a : arr) {
+            map.put(a, map.getOrDefault(a, 0) + 1);
+        }
+        List<Integer> list = new ArrayList<>(map.values());
+        Collections.sort(list, (a, b) -> a - b);// small freq first
+        int res = map.size();
+        for (int i = 0; i < list.size(); i++) {
+            k -= list.get(i);
+            if (k < 0) {
+                return res;
+            }
+            res--;
+        }
+        return res;
+    }
+}

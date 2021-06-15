6a9fb8eb1ca09a2662a22213d7fcf29fc7564709
Sai Chen <saichenlaioffer@gmail.com>
2021-06-15 14:53:52 -0400

    add IntegertoRoman.java

---
+++ Amazon/IntegertoRoman.java
@@ -0,0 +1,18 @@
+package Amazon;
+
+public class IntegertoRoman {
+    private static final int[] VALUE = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static final String[] STR = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    
+    // time: O(n) n is small so O(1) space: O(1)
+    public String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < VALUE.length && num > 0; i++) {
+            while (VALUE[i] <= num) {
+                num -= VALUE[i];
+                sb.append(STR[i]);
+            }
+        }
+        return sb.toString();
+    }
+}

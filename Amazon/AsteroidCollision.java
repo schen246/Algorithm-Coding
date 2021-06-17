9f77581b43add8f5a8d0d453048a2c72bc7b37f2
Sai Chen <saichenlaioffer@gmail.com>
2021-06-17 18:11:33 -0400

    add AsteroidCollision.java

---
+++ Amazon/AsteroidCollision.java
@@ -0,0 +1,32 @@
+package Amazon;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class AsteroidCollision {
+    // stack - time: O(n) space: O(n)
+    public int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int a : asteroids) {
+            if (a > 0) {
+                stack.push(a);
+            } else {
+                while (!stack.isEmpty() && stack.peek() > 0 && stack.peek() < -a) {
+                    stack.pop();
+                }
+                if (!stack.isEmpty() && stack.peek() == -a) {
+                    stack.pop();
+                    continue;
+                }
+                if (stack.isEmpty() || stack.peek() < 0) {
+                    stack.push(a);
+                }
+            }
+        }
+        int[] res = new int[stack.size()];
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            res[i] = stack.pop();
+        }
+        return res;
+    }
+}

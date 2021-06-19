f65628147403216452b8fac85da781520f355342
Sai Chen <saichenlaioffer@gmail.com>
2021-06-18 23:41:26 -0400

    add LongestCommonSubsequence.java

--- EditDistance.java
+++ EditDistance.java
@@ -25,4 +25,34 @@ public class EditDistance {
         }
         return dp[word1.length()][word2.length()];
     }
+
+    // M2: recursion + memo - time: O(m * n) space: O(m * n)
+    public int minDistance2(String word1, String word2) {
+        int[][] memo = new int[word1.length() + 1][word2.length() + 1];
+        return dfs(word1, word1.length(), word2, word2.length(), memo);
+    }
+    
+    private int dfs(String word1, int index1, String word2, int index2, int[][] memo) {
+        if (memo[index1][index2] != 0) {
+            return memo[index1][index2];
+        }
+        if (index1 == 0) {
+            memo[0][index2] = index2;
+            return index2;
+        }
+        if (index2 == 0) {
+            memo[index1][0] = index1;
+            return index1;
+        }
+        int res = Integer.MAX_VALUE;
+        if (word1.charAt(index1 - 1) == word2.charAt(index2 - 1)) {
+            res = dfs(word1, index1 - 1, word2, index2 - 1, memo);
+        } else {
+            res = Math.min(res, dfs(word1, index1 - 1, word2, index2, memo) + 1);// delete
+            res = Math.min(res, dfs(word1, index1, word2, index2 - 1, memo) + 1);// add
+            res = Math.min(res, dfs(word1, index1 - 1, word2, index2 - 1, memo) + 1);// replace
+        }
+        memo[index1][index2] = res;
+        return res;
+    }
 }

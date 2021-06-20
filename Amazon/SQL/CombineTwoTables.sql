1bd55578cc8f84e938033006e9a1ac918d8279bb
Sai Chen <saichenlaioffer@gmail.com>
2021-06-20 14:00:14 -0400

    add EmployeeBonus.sql

--- Amazon/SQL/CombineTwoTables.sql
+++ Amazon/SQL/CombineTwoTables.sql
@@ -1,3 +1,5 @@
-/* result should include each person, regardless if there is an address for that person*/
+/* result should include each person, regardless if there is an address for that person
+https://www.diffen.com/difference/Inner_Join_vs_Outer_Join
+*/
 
 select FirstName, LastName, City, State from Person left join Address on Person.PersonId = Address.PersonId;
---
+++ Amazon/SQL/EmployeeBonus.sql
@@ -0,0 +1,1 @@
+select Employee.name, Bonus.bonus from Employee left join Bonus on Employee.empid = Bonus.empid where Bonus.bonus < 1000 or Bonus.bonus is null;

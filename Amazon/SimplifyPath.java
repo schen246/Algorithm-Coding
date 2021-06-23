3e92344070c3102e78ee99fc978acafaa45b6cae
Sai Chen <saichenlaioffer@gmail.com>
2021-06-22 23:07:41 -0400

    add SimplifyPath.java

---
+++ Amazon/SimplifyPath.java
@@ -0,0 +1,24 @@
+package Amazon;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class SimplifyPath {
+    // stack - time: O(n) space: O(n)
+    public String simplifyPath(String path) {
+        String[] strs = path.split("/");
+        Deque<String> stack = new ArrayDeque<>();
+        for (String str : strs) {
+            if (str.equals("..") && !stack.isEmpty()) {
+                stack.pop();
+            } else if (!str.equals("") && !str.equals(".") && !str.equals("..")) {
+                stack.push(str);
+            }
+        }
+        String res = "";
+        while (!stack.isEmpty()) {
+            res = "/" + stack.pop() + res;
+        }
+        return res.equals("") ? "/" : res;
+    }
+}

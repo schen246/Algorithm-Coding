9cc320f4957cbc35851823e926169c13dd21191b
Sai Chen <saichenlaioffer@gmail.com>
2021-07-02 16:54:07 -0400

    add BinaryTreeZigzagLevelOrderTraversal.java

---
+++ Amazon/BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,34 @@
+package Amazon;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeZigzagLevelOrderTraversal {
+    // bfs - time: O(n) space: O(n)
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.offer(root);
+        int level = 0;
+        while (q.size() > 0) {
+            int size = q.size();
+            level++;
+            List<Integer> cur = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                cur.add(node.val);
+                if (node.left != null) q.offer(node.left);
+                if (node.right != null) q.offer(node.right);
+            }
+            if (level % 2 == 0) {
+                Collections.reverse(cur);
+            }
+            res.add(cur);
+        }
+        return res;
+    }
+}

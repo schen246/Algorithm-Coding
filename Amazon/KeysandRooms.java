c694960649a1e1073431724e1213d1cadd47e073
Sai Chen <saichenlaioffer@gmail.com>
2021-07-02 16:49:28 -0400

    add KeysandRooms.java

---
+++ Amazon/KeysandRooms.java
@@ -0,0 +1,27 @@
+package Amazon;
+
+import java.util.List;
+
+public class KeysandRooms {
+    // time: O(n) space: O(n)
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean[] visited = new boolean[rooms.size()];
+        dfs(rooms, 0, visited);
+        for (int i = 0; i < visited.length; i++) {
+            if (!visited[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private void dfs(List<List<Integer>> rooms, int index, boolean[] visited) {
+        if (visited[index]) {
+            return;
+        }
+        visited[index] = true;
+        for (int nei : rooms.get(index)) {
+            dfs(rooms, nei, visited);
+        }
+    }
+}

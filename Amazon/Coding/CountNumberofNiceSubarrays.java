9385c202a31d9d8b3eb62f5b81c9839b0c2d5273
Sai Chen <saichenlaioffer@gmail.com>
2021-07-22 21:31:40 -0400

    add SubarraySumEqualsK.java

---
+++ Amazon/Coding/CountNumberofNiceSubarrays.java
@@ -0,0 +1,45 @@
+package Amazon.Coding;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountNumberofNiceSubarrays {
+    // M1: res = atMost(nums, k) - atMost(nums, k - 1)
+    // time: O(n) space: O(1)
+    public int numberOfSubarrays(int[] nums, int k) {
+        return helper(nums, k) - helper(nums, k - 1);
+    }
+
+    private int helper(int[] nums, int k) {
+        int i = 0, j = 0, cnt = 0, res = 0;
+        while (j < nums.length) {
+            cnt += (nums[j] & 1);
+            while (cnt > k) {
+                cnt -= (nums[i] & 1);
+                i++;
+            }
+            res += (j - i + 1);
+            j++;
+        }
+        return res;
+    }
+
+    // M2: map - nums of subarrays with sum == k (convert to 0-1 array)
+    // time: O(n) space: O(n)
+    public int numberOfSubarrays2(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0, res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += (nums[i] & 1);
+            res += map.getOrDefault(sum - k, 0);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return res;
+    }
+
+    // M3: two pointers - nums of subarrays with sum == k (convert to 0-1 array)
+    public int numberOfSubarrays3(int[] nums, int k) {
+
+    }
+}
---
+++ Amazon/Coding/SubarraySumEqualsK.java
@@ -0,0 +1,19 @@
+package Amazon.Coding;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubarraySumEqualsK {
+    // prefix sum + map - time: O(n) space: O(n)
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0, res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            res += map.getOrDefault(sum - k, 0);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return res;
+    }
+}
